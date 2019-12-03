b587c8afc1c97d0335ffa74a6bb97f4e5981fda1
vadim-os <osinsky_vadim@yahoo.com>
2019-12-03 10:10:32 +0200

    Updates

---
+++ precache-manifest.3e0f1e396333114d310a61c133f042a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166c4c809bdc7043dc06b5f9618358b5",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "6ea624aa9b9697035ed0",
+    "url": "/cra-demo/static/js/2.a3e8d21e.chunk.js"
+  },
+  {
+    "revision": "d1136c3cd95d2bd409f0",
+    "url": "/cra-demo/static/js/main.bbc84d02.chunk.js"
+  },
+  {
+    "revision": "51dd5d735150a0d15256",
+    "url": "/cra-demo/static/js/runtime-main.30833128.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cra-demo/precache-manifest.b74812e9ab6c5a3778565dc17ee09c9e.js"
+  "/cra-demo/precache-manifest.3e0f1e396333114d310a61c133f042a7.js"
 );
 
 self.addEventListener('message', (event) => {
